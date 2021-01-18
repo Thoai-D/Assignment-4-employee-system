9b0b79a41df20ab737663073e8645dd61578e133
Thoai Do Huy <thoaidohuy@gmail.com>
2021-01-18 12:07:10 +0700

    Finished

---
+++ Assignment 4 employee system/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_4_employee_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
